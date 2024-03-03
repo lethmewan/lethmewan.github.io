1f2df93738fc034b74572d51a570dcccfa29c847
lethmewan <157079615+lethmewan@users.noreply.github.com>
2024-03-03 14:47:53 +0530

    update

--- Energizer/script.js
+++ Energizer/script.js
@@ -21,20 +21,3 @@ var tablinks=document.getElementsByClassName("tab-links");
      function closemenu(){
         sidemenu.style.right="-200px";
      }
-
-     const scriptURL = 'https://script.google.com/macros/s/AKfycbzwb5ODLglMoESpwsHAYAwGbsHbjB4K7CxJZNkPFq5bp2gX7-XXlZAMTydSHbUP2XLx_w/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg =document.getElementById("msg")
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        msg.innerHTML="Message sent succesfully"
-        setTimeout(function(){
-            msg.innerHTML=""
-        },5000)
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
