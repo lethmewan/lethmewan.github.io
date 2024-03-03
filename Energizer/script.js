430e91ee9cdda41a7273270dd451fafe45e1115c
lethmewan <157079615+lethmewan@users.noreply.github.com>
2024-03-03 16:23:56 +0530

    4

--- Energizer/script.js
+++ Energizer/script.js
@@ -1,4 +1,25 @@
-var tablinks=document.getElementsByClassName("tab-links");
+const body = document.querySelector("body"),
+    sidebar = body.querySelector(".sidebar"),
+    toggle = body.querySelector(".toggle"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+    toggle.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    });
+
+    modeSwitch.addEventListener("click", () => {
+        body.classList.toggle("dark");
+
+        if(body.classList.contains("dark")){
+            modeText.innerText="Light Mode"
+        }else{
+            modeText.innerText="Dark Mode"
+
+        }
+    });
+
+    var tablinks=document.getElementsByClassName("tab-links");
     var tabcontents=document.getElementsByClassName("tab-contents");
 
     function opentab(tabname){
@@ -21,3 +42,20 @@ var tablinks=document.getElementsByClassName("tab-links");
      function closemenu(){
         sidemenu.style.right="-200px";
      }
+
+     const scriptURL = 'https://script.google.com/macros/s/AKfycbzwb5ODLglMoESpwsHAYAwGbsHbjB4K7CxJZNkPFq5bp2gX7-XXlZAMTydSHbUP2XLx_w/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg =document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message sent succesfully"
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
